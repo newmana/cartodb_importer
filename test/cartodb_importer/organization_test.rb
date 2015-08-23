48a8e825c672bfe677c462b16bbd32fd481da089
Andrew Newman <andrewfnewman@gmail.com>
2015-08-23 13:17:29 +1000

    Fix test.

--- test/cartodb_importer/organization_test.rb
+++ test/cartodb_importer/organization_test.rb
@@ -3,11 +3,9 @@ require_relative '../test_helper'
 module CartodbImporter
 
   class OrganizationTest < Test::Unit::TestCase
-    API_KEY = (1..40).inject('') { |s, _| s + 'f' }
-
     def test_organization_url
       uri = URL_GEN.url(Organization::PATH)
-      assert_equal('http://test.com/user/suser/api/v1/org?api_key=ffffffffffffffffffffffffffffffffffffffff', uri.to_s)
+      assert_equal('http://localhost/user/yyy/api/v1/org?api_key=ffffffffffffffffffffffffffffffffffffffff', uri.to_s)
     end
   end
 end
