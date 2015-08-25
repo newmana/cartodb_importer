94a6749b866ad580f1fce00760e4650dbf1a1854
Andrew Newman <andrewfnewman@gmail.com>
2015-08-25 17:06:37 +1000

    Use the splat.

--- lib/cartodb_importer/import_shapefile.rb
+++ lib/cartodb_importer/import_shapefile.rb
@@ -11,8 +11,8 @@ module CartodbImporter
     def import_for_org(path, file_name)
       if REQUIRED_EXT.inject(true) {|c, e| c && File.exists?("#{path}/#{file_name}.#{e}") }
         # Assume zip
-        files_to_zip = REQUIRED_EXT.map { |e| "#{path}/#{file_name}.#{e}" }.join(' ')
-        Open3.capture3("7za", 'a', zip_file(path, file_name), files_to_zip)
+        files_to_zip = REQUIRED_EXT.map { |e| "#{path}/#{file_name}.#{e}" }
+        Open3.capture3('7za', 'a', zip_file(path, file_name), *files_to_zip)
         begin
           ImportFile.new(@url_gen).upload_table_for_org(zip_file(path, file_name))
         ensure
